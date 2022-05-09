c6d4ff67b293f660959af90d84f011b6081c54c0
Abdoo <abdelrahmanserageldin@gmail.com>
2022-05-09 09:13:17 +0200

    Add project files.

---
+++ Portfolio.DAL/Entites/Portfolio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DAL.Entites
+{
+    public class Portfolio
+    {
+        public Guid Id { get; set; }
+
+        public string projectName { get; set; }
+        public string projectPic { get; set; }
+
+        public string projectDesc { get; set; }
+    }
+}
---
+++ Portfolio.DAL/Entites/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DAL.Entites
+{
+    public class Profile
+    {
+        // GUID (Global Unique IDentifier)
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Picture { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
